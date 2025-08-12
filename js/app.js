c5c1efa31b68943170b6dab11483e68c07c1c1e9
SebastianNCalvo <sebaacalvoo@gmail.com>
2025-08-12 11:40:42 -0300

    Cargando elementos desde js a ofertas

--- js/app.js
+++ js/app.js
@@ -7,208 +7,111 @@
 // Dicho simulador comienza con la función de seleccionar la categoria del producto, a la cual se accede por el número de dicha categoria o por el nombre de la misma.
 // Se ejecuta una función de la categoria seleccionada, la cual recorre el array filtrando la categoria y mostrandole uno a uno los productos con su valor por unidad. Una vez seleccionados, se especifica la cantidad y son pusheados al carrito que posteriormente será mostrado, detallando el ticket de gasto.
 
-const nombre = prompt('¡Bienvenido! Ingrese su nombre por favor:');
-
-let productosAlmacen = [
-        {
-            categoria: "alfajor",
-            nombre: 'Milka Oreo',
-            precio: 1500,
-        },
-        {
-            categoria: "alfajor",
-            nombre: 'Pepito',
-            precio: 1200,
-        },
-        {
-            categoria: "alfajor",
-            nombre: 'Terrabusi',
-            precio: 1000,
-        },
-        {
-            categoria: "alfajor",
-            nombre: 'Tri shot',
-            precio: 900,
-        },
-    
-        {
-            categoria: "chocolate",
-            nombre: 'Cofler con almendras',
-            precio: 1300,
-        },
-        {
-            categoria: "chocolate",
-            nombre: 'Cofler con Rocklets',
-            precio: 1100,
-        },
-        {
-            categoria: "chocolate",
-            nombre: 'Cofler Block',
-            precio: 800,
-        },
-        {
-            categoria: "chocolate",
-            nombre: 'Cofler tres placeres',
-            precio: 500,
-        },
-        {
-            categoria: "snack",
-            nombre: 'Papas fritas Jamón serrano',
-            precio: 3000,
-        },
-        {
-            categoria: "snack",
-            nombre: 'Papas fritas sabor Ketchup',
-            precio: 2800,
-        },
-        {
-            categoria: "snack",
-            nombre: 'Papas fritas acanaladas',
-            precio: 2500,
-        },
-        {
-            categoria: "snack",
-            nombre: 'Papas fritas clasicas',
-            precio: 2200,
-        },
-        {
-            categoria: "pizza",
-            nombre: 'Pizza de Mozzarella (Incluye 2 und)',
-            precio: 10000,
-        },
-        {
-            categoria: "pizza",
-            nombre: 'Pizza de Mozarella XL',
-            precio: 12000,
-        },
-        {
-            categoria: "pizza",
-            nombre: 'Pizza de Mozarella clásica',
-            precio: 9000,
-        },
-        {
-            categoria: "pizza",
-            nombre: 'Pizza de Mozarella y jamón',
-            precio: 9500,
-        }
-]
-
-let cantidadIngresada;
-let listaDeComprasConsola = [];
-let total = 0;
-let eleccion;
-let finalCompra;
-
-function seleccionarCategoria(){
-    eleccionCategoria = prompt(`¡Hola ${nombre}! En nuestro almacen contamos con productos como: 1) Alfajores, 2) Chocolates, 3) Snacks y 4) Pizzas. ¿En cuál de dichos productos está interesado?`)
-    if(eleccionCategoria == 1 || eleccionCategoria === "Alfajores"){
-        eleccionAlfajores()
-    } else if(eleccionCategoria == 2 || eleccionCategoria === "Chocolates"){
-        eleccionChocolates()
-    } else if(eleccionCategoria == 3 || eleccionCategoria === "snack"){
-        eleccionSnack()
-    } else if(eleccionCategoria == 4 || eleccionCategoria === "pizza"){
-        eleccionPizza()      
-    } else{
-        seleccionarCategoria()
-    }
-    finalDeLaCompra()
-}
-
-function cantidadIngresadaComoEntero() {
-  do {
-    cantidadIngresada = prompt("Ingrese el número de unidades deseadas:");
-    if (cantidadIngresada === null || cantidadIngresada == undefined) {
-      return cantidadIngresada = 0;
-    }
-    cantidadIngresada = Number(cantidadIngresada);
-
-  } while (!Number.isInteger(cantidadIngresada) || cantidadIngresada < 1);
-  return cantidadIngresada;
-}
-
-function eleccionAlfajores() {
-    for(let objeto of productosAlmacen.filter(productosAlmacen => productosAlmacen.categoria === "alfajor")){
-        eleccion = prompt('Tenemos ' + objeto.nombre + ' a un precio de ' + objeto.precio +'. ¿Desea agregarlo al carrito? Indique "Si" o "No" ')
-        if (eleccion === "Si" || eleccion === "si"){
-            cantidadIngresadaComoEntero()
-            total = total + cantidadIngresada * objeto.precio
-            listaDeComprasConsola.push(objeto.categoria + ` ` + objeto.nombre + ` ${cantidadIngresada} ud ` + `$` + cantidadIngresada*objeto.precio)
-        } else if (eleccion === "No" || eleccion === "no"){
-            alert('Le muestro el siguiente producto')
-        } else {
-            alert('Opción no valida. Operación cancelada')
-        }
+class Producto {
+    constructor(id, categoria, nombre, precio, img){
+        this.id = id;
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img
     }
 }
+let alfajorArrozDDL = new Producto("alfajor-arroz-ddl", "alfajor", "Alfajor de Arroz Dulce de leche X6 Chocoarroz Paq 132 Grm", 5999, "../multimedia/productos/chocoarroz-oferta-dl.jpg");
+let alfajorArrozFrutilla = new Producto("alfajor-arroz-frutilla", "alfajor", "Alfajor de Arroz Frutilla X6 Chocoarroz Paq 132 Grm", 5999, "../multimedia/productos/chocoarroz-oferta-frutilla.jpg");
+let alfajorArrozLimon = new Producto("alfajor-arroz-limon", "alfajor", "Alfajor de Arroz Limon X6 Chocoarroz Paq 132 Grm", 5999, "../multimedia/productos/chocoarroz-oferta-limon.jpg");
+let alfajorArrozMarroc = new Producto("alfajor-arroz-marroc", "alfajor", "Alfajor de Arroz Marroc X6 Chocoarroz Paq 132 Grm", 5999, "../multimedia/productos/chocoarroz-oferta-marroc.jpg");
+let panFacilitas = new Producto("pan-facilitas", "pan", "Pan de mesa con salvado Facilitas 360gr", 2299, "../multimedia/productos/pan-lactal-facilitas.jpg");
+let panLaPerla = new Producto("pan-la-perla", "pan", "Pan de mesa La Perla 330gr", 2199, "../multimedia/productos/pan-lactal-la-perla.jpg");
+let panLacteadoMendia = new Producto("pan-mendia", "pan", "Pan de mesa lacteado Mendia 360gr", 1999, "../multimedia/productos/pan-lactal-mendia.jpg");
+let panMultiCerealNoly = new Producto("pan-noly", "pan", "Pan linea dorada multi cereal Noly 360gr", 1999, "../multimedia/productos/pan-lactal-noly.jpg");
 
-function eleccionChocolates() {
-    for(let objeto of productosAlmacen.filter(productosAlmacen => productosAlmacen.categoria === "chocolate")){
-        eleccion = prompt('Tenemos ' + objeto.nombre + ' a un precio de ' + objeto.precio +'. ¿Desea agregarlo al carrito? Indique "Si" o "No" ')
-        if (eleccion === "Si" || eleccion === "si"){
-            cantidadIngresadaComoEntero()
-            total = total + cantidadIngresada * objeto.precio
-            listaDeComprasConsola.push(objeto.categoria + ` ` + objeto.nombre + ` ${cantidadIngresada} ud ` + `$` +  cantidadIngresada*objeto.precio)
-        } else if (eleccion === "No" || eleccion === "no"){
-            alert('Le muestro el siguiente producto')
-        } else {
-            alert('Opción no valida. Operación cancelada')
-        }
-    }
-}
 
-function eleccionSnack() {
-    for(let objeto of productosAlmacen.filter(productosAlmacen => productosAlmacen.categoria === "snack")){
-        eleccion = prompt('Tenemos ' + objeto.nombre + ' a un precio de ' + objeto.precio +'. ¿Desea agregarlo al carrito? Indique "Si" o "No" ')
-        if (eleccion === "Si" || eleccion === "si"){
-            cantidadIngresadaComoEntero()
-            total = total + cantidadIngresada * objeto.precio
-            listaDeComprasConsola.push(objeto.categoria + ` ` + objeto.nombre + ` ${cantidadIngresada} ud ` + `$` +  cantidadIngresada*objeto.precio)
-        } else if (eleccion === "No" || eleccion === "no"){
-            alert('Le muestro el siguiente producto')
-        } else {
-            alert('Opción no valida. Operación cancelada')
-        }
-    }
-}
 
-function eleccionPizza() {
-    for(let objeto of productosAlmacen.filter(productosAlmacen => productosAlmacen.categoria === "pizza")){
-        eleccion = prompt('Tenemos ' + objeto.nombre + ' a un precio de ' + objeto.precio +'. ¿Desea agregarlo al carrito? Indique "Si" o "No" ')
-        if (eleccion === "Si" || eleccion === "si"){
-            cantidadIngresadaComoEntero()
-            total = total + cantidadIngresada * objeto.precio
-            listaDeComprasConsola.push(objeto.categoria + ` ` + objeto.nombre + ` ${cantidadIngresada} ud ` + `$` +  cantidadIngresada*objeto.precio)
-        } else if (eleccion === "No" || eleccion === "no"){
-            alert('Le muestro el siguiente producto')
-        } else {
-            alert('Opción no valida. Operación cancelada')
-        }
-    }
-}
+const array2x1 = [alfajorArrozDDL, alfajorArrozFrutilla, alfajorArrozLimon, alfajorArrozMarroc];
+const array20Off = [panFacilitas, panLaPerla, panLacteadoMendia, panMultiCerealNoly];
 
-function finalDeLaCompra() {
-    finalCompra = prompt('¿Desea comprar algo más? Indique "Si" o "No"')
-    if(finalCompra === "Si" || finalCompra === "si"){
-        seleccionarCategoria();
-    } else if(finalCompra === "No" || finalCompra === "no"){
-        alert(`El total es de $${total}.
-            Por consola verá impreso su ticket.
-            ¡Gracias por su compra!`)
-    } else {
-        alert(`El valor ingresado no es valido. Damos por finalizada su compra.
-        El total es de $${total}.
-        Por consola verá impreso su ticket.
-        ¡Gracias por su compra!`)
-    }
-}
 
-function ticketPorConsola() {
-    listaDeComprasConsola.push(`Total a pagar $${total}`)
-    console.log(listaDeComprasConsola)
-}
+const cargarDomOfertas = () => {
+    let divContenedor2x1 = document.getElementById("divContenedor2x1")
+    array2x1.forEach(producto => {
+        let div = document.createElement("div");
+        div.className = "col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"
+        div.innerHTML = `
+            <div class="cajita">
+                <img class="img_shadow" src="${producto.img}" alt="">
+                <p class="descripcion-fotos">${producto.nombre}</p>
+                <span>${producto.precio}</span>
+                <input type="button" value="Agregar" id="${producto.id}">                
+            </div>
+            `;
+        divContenedor2x1.appendChild(div)
+    });
 
-window.onload = function(){
-    seleccionarCategoria()
-    ticketPorConsola()
+    let divContenedor20Off = document.querySelector("#divContenedor20Off");
+    array20Off.forEach(producto => {
+        let div = document.createElement("div");
+        div.className = "col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"
+        div.innerHTML = `
+            <div class="cajita">
+                <img class="img_shadow" src="${producto.img}" alt="">
+                <p class="descripcion-fotos">${producto.nombre}</p>
+                <span>${producto.precio}</span>
+                <input type="button" value="Agregar" id="${producto.id}">                
+            </div>
+            `;
+        divContenedor20Off.appendChild(div)
+    })
+
 }
+cargarDomOfertas()
+
+let seleccionaArrozDDL = document.getElementById('alfajor-arroz-ddl');
+let seleccionaArrozFrutilla = document.getElementById('alfajor-arroz-frutilla');
+let seleccionaArrozLimon = document.getElementById('alfajor-arroz-limon');
+let seleccionaArrozMarroc = document.getElementById('alfajor-arroz-marroc');
+let seleccionaPanFacilitas = document.getElementById('pan-facilitas');
+let seleccionaPanLaPerla = document.getElementById('pan-la-perla');
+let seleccionaPanMendia = document.getElementById('pan-mendia');
+let seleccionaMultiCerealNoly = document.getElementById('pan-noly');
+
+
+
+seleccionaArrozDDL.addEventListener('click', function(){
+    alert(`Seleccionaste ${alfajorArrozDDL.nombre}`)
+    localStorage.setItem(`${alfajorArrozDDL.nombre}`, `${alfajorArrozDDL.precio}`)
+})
+
+seleccionaArrozFrutilla.addEventListener('click', function(){
+    alert(`Seleccionaste ${alfajorArrozFrutilla.nombre}`)
+    localStorage.setItem(`${alfajorArrozFrutilla.nombre}`, `${alfajorArrozFrutilla.precio}`)
+})
+
+seleccionaArrozLimon.addEventListener('click', function(){
+    alert(`Seleccionaste ${alfajorArrozLimon.nombre}`)
+    localStorage.setItem(`${alfajorArrozLimon.nombre}`, `${alfajorArrozLimon.precio}`)
+})
+
+seleccionaArrozMarroc.addEventListener('click', function(){
+    alert(`Seleccionaste ${alfajorArrozMarroc.nombre}`)
+    localStorage.setItem(`${alfajorArrozMarroc.nombre}`, `${alfajorArrozMarroc.precio}`)
+})
+
+seleccionaPanFacilitas.addEventListener('click', function(){
+    alert(`Seleccionaste ${panFacilitas.nombre}`)
+    localStorage.setItem(`${panFacilitas.nombre}`, `${panFacilitas.precio}`)
+})
+
+seleccionaPanLaPerla.addEventListener('click', function(){
+    alert(`Seleccionaste ${panLaPerla.nombre}`)
+    localStorage.setItem(`${panLaPerla.nombre}`, `${panLaPerla.precio}`)
+})
+
+seleccionaPanMendia.addEventListener('click', function(){
+    alert(`Seleccionaste ${panLacteadoMendia.nombre}`)
+    localStorage.setItem(`${panLacteadoMendia.nombre}`, `${panLacteadoMendia.precio}`)
+})
+
+seleccionaMultiCerealNoly.addEventListener('click', function(){
+    alert(`Seleccionaste ${panMultiCerealNoly.nombre}`)
+    localStorage.setItem(`${panMultiCerealNoly.nombre}`, `${panMultiCerealNoly.precio}`)
+})
